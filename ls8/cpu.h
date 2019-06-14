79a659904ac482afa4fe16f31e21688dc6b19f57
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-06-14 10:49:21 -0500

    Instructions

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -29,6 +29,10 @@ enum alu_op {
 #define PUSH 0b01000101
 #define CALL 0b01010000
 #define RET  0b00010001
+#define CMP  0b10100111
+#define JMP  0b01010100
+#define JEQ  0b01010101
+#define JNE  0b01010110
 
 // TODO: more instructions here. These can be used in cpu_run().
 
@@ -39,3 +43,5 @@ extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
 
 #endif
+
+
